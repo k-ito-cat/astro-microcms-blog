dfcee56c1a9aa4a1df5a81ecd8dadc8315f8459b
k-ito-cat <kyohy163@gmail.com>
2025-02-08 20:11:46 +0900

    feat: 動的な詳細ページを作成

--- src/libs/microcms.ts
+++ src/libs/microcms.ts
@@ -1,11 +1,19 @@
-import { createClient, type MicroCMSQueries } from "microcms-js-sdk";
+import {
+  createClient,
+  type MicroCMSListContent,
+  type MicroCMSQueries,
+} from "microcms-js-sdk";
 
-console.log(import.meta.env.MICROCMS_SERVICE_DOMAIN);
 
 const client = createClient({
   serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN as string, // 環境変数があるという前提
   apiKey: import.meta.env.MICROCMS_API_KEY as string,
 });
+
+export type Blog = {
+  title: string;
+  content: string;
+} & MicroCMSListContent;
 
 export const getBlogs = async (queries?: MicroCMSQueries) => {
   return await client.getList({ endpoint: "blogs", queries });
