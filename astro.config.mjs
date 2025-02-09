4059bd740b1a043f4de56e953fb1bc7f65d1369d
k-ito-cat <kyohy163@gmail.com>
2025-02-09 14:51:44 +0900

    add(ui): tailwind v4導入とastro v5.2へのアップグレード

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   output: "server",
   integrations: [mdx(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
